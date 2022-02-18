86dd055f52d7595d5910cc66bd5e81120b58b018
timothy101000 <97958832+timothy101000@users.noreply.github.com>
2022-02-18 20:12:12 +0300

    Add files via upload

---
+++ Library Project Milestone/main.c
@@ -0,0 +1,22 @@
+/*
+     Project: County Library Management System.
+     Author:  Timothy karingo
+     Date:    Feb, 2022
+     Compiler: GNU GCC
+     Language: C99
+     License: MIT
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    printf("\County Library\n");
+    printf("Welcome Mr. Timothy. \n");
+    printf("What would you like to do?/n");
+    printf("1. Add new user\n");
+    printf("2. Add new book.\n");
+    printf("Selected action: ");
+    return 0;
+}
